f77e7f4fdd5cdd88530b88b6ecb02fa4345138c2
Vidhi425 <hcpsvidhi25@gmail.com>
2025-06-21 14:23:50 +0530

    remove build

--- build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appteslaDashboard_res.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appteslaDashboard")
-set(working_dir "C:/Project/opencv/teslaDashboard 2/teslaDashboard")
-set(src_and_dest_list
-    "C:/Project/opencv/teslaDashboard 2/teslaDashboard/res.qrc"
-    "C:/Project/opencv/teslaDashboard 2/teslaDashboard/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/teslaDashboard/res.qrc"
-
-)
-set(timestamp_file "C:/Project/opencv/teslaDashboard 2/teslaDashboard/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/appteslaDashboard_res.txt")
